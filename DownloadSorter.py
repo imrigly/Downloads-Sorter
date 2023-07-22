0b5b06136f830de6d6bcd2fba00c082eeffb635f
imrigly <140164790+imrigly@users.noreply.github.com>
2023-07-22 18:37:31 +0300

    functionality added

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -2,21 +2,54 @@ import keyboard
 import glob
 import shutil
 
+FileTypes: dict =  {
+    "exe":"Executables",
+    "msi":"Executables",
+    "torrent":"Torrents",
+    "png":"Images",
+    "svg":"Images",
+    "jpg":"Images",
+    "jpeg":"Images",
+    "gif":"Images",
+    "zip":"Compressed Files",
+    "7z":"Compressed Files",
+    "rar":"Compressed Files",
+    "wav":"Audio",
+    "mp3":"Audio",
+    "flac":"Audio",
+    "m4a":"Audio",
+    "mkv":"Videos",
+    "avi":"Videos",
+    "mp4":"Videos",
+    "webm":"Videos",
+    "stl":"3D"
+}
+
+#opens "username" file
 usernamefile = open('username.txt', 'r+')
  
 def sortfiles(Extension, FolderName):
-    Files = glob.glob('c:/Users/imrig/Downloads/*.exe')
+    
+    #extention detection
+    Files = glob.glob('c:/Users/' + username + '/Downloads/*.' + Extension)
+    
+    #sorting loop
     for i in Files:
         NewName = i.split('\\')[-1]
-        NewPath = ("c:/Users/imrig/Downloads/" + username + "/" + NewName)
-        print(NewName)
-        print(i)
-        print(NewPath)
+        NewPath = ("c:/Users/" + username + "/Downloads/" + FolderName + "/" + NewName)
         shutil.move(i, NewPath)
     
+#good practice
 if __name__ == '__main__':
+    
+    #username check
     if usernamefile.read(1) == '':
         username = input('Please input your username: ')
-        usernamefile.write(username)
+        usernamefile.write(' ' + username)
     else: username = usernamefile.read()
-    
+
+    #filesort
+    if len(username) > 0:
+        for Extension, FolderName in FileTypes.items():
+            sortfiles(Extension, FolderName)
+        
