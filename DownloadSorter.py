6f3ac3ddc5f57bf782de2449011e38748608bfe5
imrigly <140164790+imrigly@users.noreply.github.com>
2023-07-22 19:31:34 +0300

    automatic folder creation added

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -1,12 +1,31 @@
 import keyboard
 import glob
-import shutil
+import os
 from DictTypes import FileTypes
 
-#opens "username" file
-usernamefile = open('username.txt', 'r+')
- 
-def sortfiles(Extension, FolderName):
+#username declaration
+username: str
+
+#checks if username.txt is not empty
+def usernameCheck():  
+    
+    #opens "username" file
+    usernamefile = open('username.txt', 'r+')
+    
+    #username check
+    if usernamefile.read(1) == '':
+        username = input('[SYSTEM]: Please input your username: ')
+        usernamefile.write(' ' + username)
+    else: username = usernamefile.read()
+    
+    #allows assignment
+    return username
+
+#usermane assignment
+username = usernameCheck()
+
+#sorts files
+def fileSort(Extension, FolderName):
     
     #extention detection
     Files = glob.glob(f"c:/Users/{username}/Downloads/*.{Extension}")
@@ -15,22 +34,30 @@ def sortfiles(Extension, FolderName):
     for i in Files:
         NewName = i.split('\\')[-1]
         NewPath = (f"c:/Users/{username}/Downloads/{FolderName}/{NewName}")
-        shutil.move(i, NewPath)
-        print(f"Moved {NewName} to {FolderName}")
-    print(f"{Extension} sort completed!")
+        os.replace(i, NewPath)
+        print(f"[ACTION]: Moved {NewName} to {FolderName}")
+    print(f"[SORT]: {Extension} sort completed!")
 
+#checks if neccesary folder exist
+def folderCheck():
+    for FolderName in FileTypes.values():
+        if not os.path.exists(f"c:/Users/{username}/Downloads/{FolderName}"):
+            os.makedirs(f"c:/Users/{username}/Downloads/{FolderName}")
+            print(f"[FOLDERS]: {FolderName} folder created successfully!")
+        else: continue
+ 
+#calls fileSort for every extention type 
+def sortCall():
     
-#good practice
-if __name__ == '__main__':
-    
-    #username check
-    if usernamefile.read(1) == '':
-        username = input('Please input your username: ')
-        usernamefile.write(' ' + username)
-    else: username = usernamefile.read()
-
-    #filesort
+    #calls fileSort until completion
     if len(username) > 0:
         for Extension, FolderName in FileTypes.items():
-            sortfiles(Extension, FolderName)
-        print("All done!")
+            fileSort(Extension, FolderName)
+        print("[COMPLETE]: All done!")
+        
+#good practice
+if __name__ == '__main__':
+    folderCheck()
+    sortCall()
+    
+    
